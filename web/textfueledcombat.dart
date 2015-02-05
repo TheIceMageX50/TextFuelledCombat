5d327c5de525cbb720ae1a080a503991a5ed4a21
Aaron <palkiax50@topmail.ie>
2015-02-05 02:08:55 +0000

    Add tile images and code to render map from map data

--- web/textfueledcombat.dart
+++ web/textfueledcombat.dart
@@ -18,21 +18,22 @@ class Tfc
   {
     map = new GameMap(32, 32);
     
-    for (int i = 0; i < 32; i++) {
+    /*for (int i = 0; i < 32; i++) {
       for (int j = 0; j < 32; j++) {
         _addTile(i, j);
       }
-    }
+    }*/
     Game game = new Game(800, 600, AUTO, 'canvasDiv');
+    game.stage.setBackgroundColor(0xADD8E6);
     State state, state2; 
-    state = new FileWaitState();
+    state = new FileWaitState(map);
     game.state.add('wait', state);
     game.state.start('wait');
     
     //InputElement ie = querySelector('#test');
     //while(ie.files.isEmpty); //loop until file is submitted
     
-    state2 = new MapRenderState();
+    state2 = new MapRenderState(map);
     game.state.add('maprender', state2);
     //game.state.start('maprender');
   }
@@ -51,20 +52,47 @@ class Tfc
 //Currently this is a state for testing rendering stuff
 class MapRenderState extends State
 {
+  GameMap map;
+  
+  MapRenderState(GameMap map)
+  {
+    this.map = map;
+  }
+  
   preload()
   {
-    game.load.image('wood', 'wood_ph.png');
-    game.load.image('void', 'void_ph.png');
-  }
+    game.load.image('dirt', 'dirt.png');
+    game.load.image('dryland', 'wood_ph.png');
+    game.load.image('grass', 'grass.png');
+    game.load.image('lava', 'lava.png');
+    game.load.image('void', 'void.png');
+    game.load.image('water', 'water.png');
+    game.load.image('wood', 'wood_tile.png');
+  } 
   
   create()
   {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
-        if (j % 2 == 0) {
-          game.add.sprite(j * 32, i * 32, 'wood');
-        } else {
-          game.add.sprite(j * 32, i * 32, 'void');
+    //Hide input element as it is no longer needed since text file has already
+    //been given at this point.
+    InputElement ie = querySelector('#test');
+    ie.style.display = 'none';
+    
+    for (int i = 0; i < 20; i++) {
+      for (int j = 0; j < 20; j++) {
+        switch(map.whatTile(i, j)) {
+          case TileType.DIRT: game.add.sprite(j * 32, i * 32, 'dirt');
+          break;
+          case TileType.DRY_LAND: game.add.sprite(j * 32, i * 32, 'dryland');
+          break;
+          case TileType.GRASS: game.add.sprite(j * 32, i * 32, 'grass');
+          break;
+          case TileType.LAVA: game.add.sprite(j * 32, i * 32, 'lava');
+          break;
+          case TileType.VOID: game.add.sprite(j * 32, i * 32, 'void');
+          break;
+          case TileType.WATER: game.add.sprite(j * 32, i * 32, 'water');
+          break;
+          case TileType.WOOD_TILE: game.add.sprite(j * 32, i * 32, 'wood');
         }
       }
     }
@@ -76,6 +104,11 @@ class FileWaitState extends State
   GameMap map;
   InputElement ie;
   
+  FileWaitState(GameMap map)
+  {
+    this.map = map;
+  }
+  
   create()
   {
     TextStyle style = new TextStyle(font: "65px Arial", fill: "#ffffff", align: "center");
@@ -85,16 +118,15 @@ class FileWaitState extends State
   update()
   {
     if (ie.files.isNotEmpty) {
-      map.fileProcessor.analyseTxtFile(ie.files[0])
+      map.generateMap(ie.files[0])
       .then((_) {
         game.state.start('maprender');
       });
     }
   }
   
-  init([GameMap theMap])
+  init([args])
   {
-    map = theMap;
     ie  = querySelector('#test');
   }
 }
